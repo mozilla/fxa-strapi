88e2aa8ff4a8eb9cb449d7f549995806f333fe7b
julianpoyourow <julian@poyourow.com>
2024-11-12 18:43:03 +0000

    chore: update to strapi 5.3.0

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,66 +1,10 @@
-import type { Struct, Schema } from '@strapi/strapi';
-
-export interface StripeStripePromoCodes extends Struct.ComponentSchema {
-  collectionName: 'components_codes_stripe_promo_codes';
-  info: {
-    displayName: 'Stripe Promo Codes';
-    description: '';
-  };
-  attributes: {
-    PromoCode: Schema.Attribute.String &
-      Schema.Attribute.SetMinMaxLength<{
-        minLength: 2;
-      }>;
-  };
-}
-
-export interface StripeStripePlanChoices extends Struct.ComponentSchema {
-  collectionName: 'components_stripe_stripe_plan_choices';
-  info: {
-    displayName: 'Stripe Plan Choices';
-    description: '';
-  };
-  attributes: {
-    stripePlanChoice: Schema.Attribute.String &
-      Schema.Attribute.SetMinMaxLength<{
-        minLength: 2;
-      }>;
-  };
-}
-
-export interface StripeStripeLegacyPlans extends Struct.ComponentSchema {
-  collectionName: 'components_stripe_stripe_legacy_plans';
-  info: {
-    displayName: 'Stripe Legacy Plans';
-    description: '';
-  };
-  attributes: {
-    stripeLegacyPlan: Schema.Attribute.String &
-      Schema.Attribute.SetMinMaxLength<{
-        minLength: 1;
-      }>;
-  };
-}
-
-export interface IapGoogleSkUs extends Struct.ComponentSchema {
-  collectionName: 'components_iap_google_s_kuses';
-  info: {
-    displayName: 'Google SKUs';
-    description: '';
-  };
-  attributes: {
-    googleSKU: Schema.Attribute.String &
-      Schema.Attribute.SetMinMaxLength<{
-        minLength: 2;
-      }>;
-  };
-}
+import type { Schema, Struct } from '@strapi/strapi';
 
 export interface IapAppleProductIDs extends Struct.ComponentSchema {
   collectionName: 'components_iap_apple_product_i_ds';
   info: {
-    displayName: 'Apple Product IDs';
     description: '';
+    displayName: 'Apple Product IDs';
   };
   attributes: {
     appleProductID: Schema.Attribute.String &
@@ -70,14 +14,70 @@ export interface IapAppleProductIDs extends Struct.ComponentSchema {
   };
 }
 
+export interface IapGoogleSkUs extends Struct.ComponentSchema {
+  collectionName: 'components_iap_google_s_kuses';
+  info: {
+    description: '';
+    displayName: 'Google SKUs';
+  };
+  attributes: {
+    googleSKU: Schema.Attribute.String &
+      Schema.Attribute.SetMinMaxLength<{
+        minLength: 2;
+      }>;
+  };
+}
+
+export interface StripeStripeLegacyPlans extends Struct.ComponentSchema {
+  collectionName: 'components_stripe_stripe_legacy_plans';
+  info: {
+    description: '';
+    displayName: 'Stripe Legacy Plans';
+  };
+  attributes: {
+    stripeLegacyPlan: Schema.Attribute.String &
+      Schema.Attribute.SetMinMaxLength<{
+        minLength: 1;
+      }>;
+  };
+}
+
+export interface StripeStripePlanChoices extends Struct.ComponentSchema {
+  collectionName: 'components_stripe_stripe_plan_choices';
+  info: {
+    description: '';
+    displayName: 'Stripe Plan Choices';
+  };
+  attributes: {
+    stripePlanChoice: Schema.Attribute.String &
+      Schema.Attribute.SetMinMaxLength<{
+        minLength: 2;
+      }>;
+  };
+}
+
+export interface StripeStripePromoCodes extends Struct.ComponentSchema {
+  collectionName: 'components_codes_stripe_promo_codes';
+  info: {
+    description: '';
+    displayName: 'Stripe Promo Codes';
+  };
+  attributes: {
+    PromoCode: Schema.Attribute.String &
+      Schema.Attribute.SetMinMaxLength<{
+        minLength: 2;
+      }>;
+  };
+}
+
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
-      'stripe.stripe-promo-codes': StripeStripePromoCodes;
-      'stripe.stripe-plan-choices': StripeStripePlanChoices;
-      'stripe.stripe-legacy-plans': StripeStripeLegacyPlans;
-      'iap.google-sk-us': IapGoogleSkUs;
       'iap.apple-product-i-ds': IapAppleProductIDs;
+      'iap.google-sk-us': IapGoogleSkUs;
+      'stripe.stripe-legacy-plans': StripeStripeLegacyPlans;
+      'stripe.stripe-plan-choices': StripeStripePlanChoices;
+      'stripe.stripe-promo-codes': StripeStripePromoCodes;
     }
   }
 }
