c77647fac6579f67d51cc1538fdcd914ec40cb4f
elizabeth-ilina <eilina@mozilla.com>
2025-10-21 13:12:27 -0400

    feat(payments-next): Update fxa-strapi repo: Add new Churn Intervention content type Because:
    * Relying parties would like to include a custom message to reduce churn.
    
    This commit:
    
    * Adds required fields as per ticket outline.
    
    Closes #PAY-3357

---
+++ src/api/churn-intervention/controllers/churn-intervention.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * churn-intervention controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::churn-intervention.churn-intervention');
